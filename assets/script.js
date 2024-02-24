707c39d380bc6d44cf08f3ebbb37d15c0e6c1d73
ryan <ryanpburnett@yahoo.com>
2024-02-24 09:54:17 -0600

    first commit

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+const l = (log) => {
+    console.log(log);
+}
+
+let key = "60b91a70-b0fa-46f0-af94-6a631c4726a6";
+let word = "bad"
+let url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${key}`;
+
+let urlLiteral = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/bad?key=60b91a70-b0fa-46f0-af94-6a631c4726a6";
+
+// WORDLE TOOL
+
+// write a program that takes any given sequence of x letters and returns all words that can be spelled with just those letters
+
+// alternatively, the program can also return all words that don't contain these letters
+
+// in either functionality, the program will have a filter to only list the most common y words from the list
+
